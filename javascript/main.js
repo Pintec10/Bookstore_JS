886480f1c209f134076145a734a4afd455ee861e
ROBERTO MILANI <robimilani@gmail.com>
2019-09-09 17:12:08 +0200

    completed; added ligthbox and search, made semi-responsive

--- javascript/main.js
+++ javascript/main.js
@@ -6,19 +6,19 @@ function fetchData() {
         return resp.json();
     }).then(function (json) {
         data = json.books;
-        console.log("data after fetch");
-        console.log(data);
     }).then(function () {
         pageExecution();
     });
 }
 
 
-function buildShowcase() {
+function buildShowcase(arr) {
     let bookcase = document.getElementById("bookcase");
+    bookcase.innerHTML = "";
+    let rowHeight = 0;
+    let coverHeight = 0;
 
-
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         let card = document.createElement("div");
         let cardInner = document.createElement("div");
         let cardFront = document.createElement("div");
@@ -26,44 +26,58 @@ function buildShowcase() {
         let cardBack = document.createElement("div");
         let title = document.createElement("h2");
         let description = document.createElement("p");
+        let buttonLink = document.createElement("a");
         let button = document.createElement("button");
 
         card.className = "flip-card";
         cardInner.className = "flip-card-inner";
         cardFront.className = "flip-card-front";
         cover.className = "img-fluid";
-        cardBack.className = "flip-card-back d-flex flex-column justify-content-center align-items-center p-3";
+        cardBack.className = "flip-card-back d-flex flex-column justify-content-between p-3 overflow-auto";
         button.className = "button-style bg-danger text-white rounded"
 
-        cover.src = data[i].cover;
-        title.innerHTML = data[i].title;
-        description.innerHTML = data[i].description;
-        button.innerHTML = "more information";
+        cover.src = arr[i].cover;
+        title.innerHTML = arr[i].title;
+        description.innerHTML = arr[i].description;
+        buttonLink.setAttribute("data-fancybox", "gallery");
+        buttonLink.setAttribute("data-caption", arr[i].title);
+        buttonLink.href = arr[i].detail;
+        button.innerHTML = "more info";
 
         cardFront.append(cover);
-        cardBack.append(title, description, button);
-        cardInner.append(cover, cardFront, cardBack);
+        buttonLink.append(button);
+        cardBack.append(title, description, buttonLink);
+        cardInner.append(cardFront, cardBack);
         card.append(cardInner);
-
         bookcase.append(card);
+
+        // giving "flip-card-inner" a height equal to the one that the image has inside the flexbox
+        // (this avoids rows overlapping with each other)
+        coverHeight = cardFront.querySelector(".img-fluid").height; //GOOD
+        card.querySelector(".flip-card-inner").style.height = coverHeight + "px"; //GOOD
+        //card.querySelector(".flip-card-back").style.height = coverHeight + "px";
+        //card.querySelector(".flip-card-front").style.height = coverHeight + "px";
     }
-    console.log(bookcase);
 }
 
-
-
-
-
-
-
+function filterBooks() {
+    console.log("Input value is: ")
+    console.log(document.getElementById("search-field").value);
+    let searchInput = document.getElementById("search-field").value;
+    displayedData = data.filter(x => x.title.includes(searchInput) || x.description.includes(searchInput));
+    buildShowcase(displayedData);
+}
 
 
 
 
 
 //FINAL EXECUTION
-fetchData();
 function pageExecution() {
     console.log("executing!");
-    buildShowcase();
+    buildShowcase(data);
+    document.getElementById("search-field").addEventListener("keyup", filterBooks);
+    window.addEventListener("resize", filterBooks);
 }
+
+fetchData();
